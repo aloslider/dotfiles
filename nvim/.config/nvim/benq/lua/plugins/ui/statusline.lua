095149c703a6e5c4312857bd50cca9d734526db3
aloslider <53711835+aloslider@users.noreply.github.com>
2026-03-08 00:39:51 +0300

    [nvim] Add plugins
    - gistigns
    - screenkey
    - lualine

--- nvim/.config/nvim/benq/lua/plugins/ui/statusline.lua
+++ nvim/.config/nvim/benq/lua/plugins/ui/statusline.lua
@@ -1,5 +1,12 @@
 return {
-	'nvim-mini/mini.statusline',
-	version = '*',
-	opts = {}
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", 'diff' },
+			lualine_y = { "diagnostics" },
+			lualine_z = { "location" }
+		},
+	},
 }
