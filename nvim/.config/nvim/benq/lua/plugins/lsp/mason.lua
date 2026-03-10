577ac215a494d714bd59ec91c7ebe04c254fccbc
aloslider <53711835+aloslider@users.noreply.github.com>
2026-03-10 14:25:58 +0300

    [nvim] Add mason-dap

--- nvim/.config/nvim/benq/lua/plugins/lsp/mason.lua
+++ nvim/.config/nvim/benq/lua/plugins/lsp/mason.lua
@@ -1,25 +1,39 @@
 return {
-	"mason-org/mason-lspconfig.nvim",
-	opts = {
-		ensure_installed = {
-			"docker_compose_language_service",
-			"lua_ls",
-			"html",
-			-- "roslyn", install with :MasonInstall
-			"vtsls",
-			"vue_ls",
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = { "neovim/nvim-lspconfig", },
+		opts = {
+			ensure_installed = {
+				"docker_compose_language_service",
+				"lua_ls",
+				"html",
+				"netcoredbg",
+				-- "roslyn", install with :MasonInstall
+				"vtsls",
+				"vue_ls",
+			},
 		},
 	},
-	dependencies = {
-		"neovim/nvim-lspconfig",
-		{
-			"mason-org/mason.nvim",
-			opts = {
-				registries = {
-					"github:mason-org/mason-registry",
-					"github:Crashdummyy/mason-registry",
-				},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		dependencies = { "neovim/nvim-lspconfig", },
+		opts = {
+			ensure_installed = {
+				"coreclr",
 			}
-		},
+		}
 	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "mason-org/mason.nvim", }
+	},
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			registries = {
+				"github:mason-org/mason-registry",
+				"github:Crashdummyy/mason-registry",
+			},
+		}
+	}
 }
