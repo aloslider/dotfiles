77ba916a5045591c2466df2a81b41f7cffacd497
aloslider <53711835+aloslider@users.noreply.github.com>
2026-03-12 02:44:43 +0300

    [nvim] Replace mini.jump2d with flash for remote; Add mini.jump;

--- nvim/.config/nvim/benq/lua/plugins/editor/jump.lua
+++ nvim/.config/nvim/benq/lua/plugins/editor/jump.lua
@@ -1,5 +1,20 @@
 return {
-	"nvim-mini/mini.jump2d",
-	version = false,
-	opts = {}
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+			{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+			{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+			{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+			{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+		},
+	},
+	{
+		"nvim-mini/mini.jump",
+		version = false,
+		opts = {}
+	}
 }
